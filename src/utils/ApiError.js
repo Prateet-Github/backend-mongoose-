b0fba56f37ea114b3b1808929ee1673e58bb84d4
Prateet Tiwari <prateetiwari29@gmail.com>
2025-05-13 23:20:19 +0530

    utils code discussion

---
+++ src/utils/ApiError.js
@@ -0,0 +1,31 @@
+class ApiError extends Error {
+  constructor(message = 'something went wrong', statusCode, errors = [], stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.success = false;
+    this.errors = errors;
+    this.message = message;
+    
+// if (stack) {
+//       this.stack = stack;
+//     }
+//     else {
+//       Error.captureStackTrace(this, this.constructor);
+//     }
+
+  }
+}
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+//       res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message,
+//       })
+//     })
+//   }
+// }
+
+export default ApiError
+// export { asyncHandler }
