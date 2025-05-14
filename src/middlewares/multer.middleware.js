919fdca17e8be66706fe3658f9a04376e52a6cdc
Prateet Tiwari <prateetiwari29@gmail.com>
2025-05-15 00:23:39 +0530

    95% pre production done

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from 'multer';    // Import the multer library for handling file uploads
+
+const storage = multer.diskStorage({     // Configure storage for uploaded files in this casr ite the disk of your machine
+
+  destination: function (req, file, cb) {  
+    cb(null, './public/temp') // Set the destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename) // Set the filename for the uploaded file
+  }
+})
+
+export const upload = multer({ storage: storage })  // Create a multer instance with the defined storage configuration
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary'     // Importing the Cloudinary library
+import fs from 'fs'
+
+// Configuration for Cloudinary
+
+        cloudinary.config({ 
+                cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                api_key: process.env.CLOUDINARY_API_KEY,
+                api_secret: process.env.CLOUDINARY_API_SECRET 
+            });
+            
+          // Upload an image to Cloudinary
+
+          const uploadOnCloudinary = async (localFilePath ) => {
+              const uploadResult = await cloudinary.uploader    //  Uploading a file to Cloudinary
+                .upload(localFilePath, {
+                    public_id: 'shoes',
+                    resource_type: 'auto', // Automatically detect the resource type
+                })
+                .catch((error) => {
+                    fs.unlinkSync(localFilePath); // Delete the local file if upload fails
+                    return null;
+                });
+          
+              return uploadResult; // Return the result of the upload
+          }
+        
+
+export { uploadOnCloudinary } // Export the function for use in other modules
+    
